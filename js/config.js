0c73d5096a9f6c92928f5131b5f5d5f57c7beec3
AhmedRabia0034 <ahm20210034@std.psut.edu.jo>
2025-05-24 12:41:31 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_fKC2bOFqw', 
+        userPoolClientId: '73rcgg50gobm21ein0ffsdahoj',
+        region: 'us-east-1' 
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
